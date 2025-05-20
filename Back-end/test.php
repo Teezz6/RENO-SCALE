1da7e3000f88a0c234b4673c991ea3a421fd2ec6
erine2007 <erinegracem@gmail.com>
2025-05-20 17:46:07 +0200

    Implémentation BDD & test connection

---
+++ Back-end/test.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'config/database.php';
+
+$db = new Database();
+$conn = $db->getConnection();
+
+if ($conn) {
+    echo "Connexion réussie à la base de données ✅";
+} else {
+    echo "Échec de la connexion ❌";
+}
+?>
